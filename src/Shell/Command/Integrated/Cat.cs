8df5cf19f28fd2510ea05cc8a1c5580cb3db97f8
Evgeny <eugenysvirin@gmail.com>
2023-03-08 19:41:36 +0300

    Add: Cat.cs Wc.cs

--- src/Shell/Command/Integrated/Cat.cs
+++ src/Shell/Command/Integrated/Cat.cs
@@ -7,9 +7,19 @@ class CatCommand : Command
     public CatCommand(TextReader i, TextWriter o, ShellEnvironment e)
         : base(i, o, e) { }
 
-    protected override int Go(string[] args)
-    {
-        StdOut.WriteLine("Not implemented!");
-        return -1;
+    protected override int Go(string[] args) 
+    {   
+        foreach (var arg in args)
+        {
+            try
+            {
+                var content = File.ReadAllLines(arg);
+                StdOut.WriteLine(string.Join("\n", content));
+            }
+            catch (FileNotFoundException) {
+                StdOut.WriteLine("cat: " + arg + " No such file or directory");
+            }
+        }
+        return 0;
     }
 }
--- src/Shell/Command/Integrated/Wc.cs
+++ src/Shell/Command/Integrated/Wc.cs
@@ -9,7 +9,31 @@ class WcCommand : Command
 
     protected override int Go(string[] args)
     {
-        StdOut.WriteLine("Not implemented!");
-        return -1;
+ foreach (var arg in args)
+        {
+            try 
+            {
+                var content = File.ReadLines(arg);
+                var byteContent = File.ReadAllBytes(arg);
+
+                int linesCount = 0;
+                int bytesCount = byteContent.Length;                
+                int wordsCount = 0;
+                foreach (var line in content) 
+                {
+                    linesCount += 1;
+                    wordsCount += line.Split().Length;
+                }
+
+                StdOut.WriteLine(linesCount.ToString() + " " +
+                                 wordsCount.ToString() + " " +
+                                 bytesCount.ToString() + " " + arg);
+            }
+            catch (FileNotFoundException)
+            {
+                StdOut.WriteLine("cat: " + arg + " No such file or directory");
+            }
+        }
+        return 0;
     }
 }
