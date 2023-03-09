7b99746c76cff3da785328342016527819aa2697
grigory <gregra@mail.ru>
2023-03-09 22:38:37 +0300

    Add UI documentation

--- src/Shell/UI.cs
+++ src/Shell/UI.cs
@@ -1,14 +1,18 @@
-
-using System.Text;
-
 namespace Shell;
 
 using System;
 
+/// <summary>
+///  Класс UI отвечает за заимодействие с пользователем: вывод приветствия (prompt) и считывание команды.
+/// </summary>
 public class UI
 {
     private readonly string prompt = "$ ";
     
+    /// <summary>
+    /// Список состояний, используемый для проверки переносов строки в пользовательском вводе.
+    /// Строка переносится, если в команде присутствуют незакрытые кавычки или если в конце команды стоит обратный слеш.
+    /// </summary>
     public enum State
     {
         Normal,
@@ -16,7 +20,19 @@ public class UI
         DoubleQuote,
         SingleQuote
     }
-
+    
+    /// <summary>
+    /// Метод проверяет, закончена ли введенная пользователем команда, или она должна быть продолжена на следующей
+    /// строке. Используется методом GetCommand для накопления многострочного ввода. 
+    /// </summary>
+    /// <param name="request">Введенная пользователем стркоа</param>
+    /// <returns>Состояние введенной команды:
+    /// <list type="bullet"></list>
+    /// <item>State.Normal, если ввод завершен;</item>
+    /// <item>State.Backslash, если в конце команды был обратный слеш, который соответствует переносу строки;</item>
+    /// <item>State.SingleQuote, если в команде присутствует незакрытая одинарная кавычка;</item>
+    /// <item>State.DoubleQuote, если в команде присутствует незакрытая двойная кавычка.</item>
+    /// </returns>
     public State CheckLineBreak(string request)
     {
         Stack<State> s = new Stack<State>();
@@ -90,7 +106,11 @@ public class UI
 
         return s.Any() ? s.Peek() : State.Normal;
     }
-
+    
+    /// <summary>
+    /// Метод, принимающий пользовательский ввод, пока он не будет завершен.
+    /// </summary>
+    /// <returns>Возвращает введенную пользователем команду в виде строки.</returns>
     public string? GetCommand()
     {
         Console.ForegroundColor = ConsoleColor.White;
@@ -116,18 +136,15 @@ public class UI
             }
             request += Console.ReadLine();
             state = CheckLineBreak(request);
-
         }
 
         return request;
     }
 
-    public void PrintText(string message)
-    {
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine(message);
-    }
-
+    /// <summary>
+    /// Метод для вывода на экран текста ошибки. Текст ошибки подсвечивается специальным цветом.
+    /// </summary>
+    /// <param name="message">Текст ошибки.</param>
     public void PrintError(string message)
     {
         Console.ForegroundColor = ConsoleColor.Red;
