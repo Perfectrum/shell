5f5e6c9534884195743ec70270e0124c38182669
Evgeny <eugenysvirin@gmail.com>
2023-03-11 03:17:30 +0300

    merge conflict resolve

--- src/Shell/Command/Integrated/Cat.cs
+++ src/Shell/Command/Integrated/Cat.cs
@@ -9,10 +9,8 @@ public class CatCommand : Command
 
     public override int Go(string[] args)
     {
-<<<<<<< HEAD
+
         int returnCode = 0;
-=======
->>>>>>> 15ad7acafcbad003e5516455dcaea28e09c69a11
         foreach (var arg in args)
         {
             try
@@ -29,16 +27,9 @@ public class CatCommand : Command
             catch (FileNotFoundException)
             {
                 StdOut.WriteLine("cat: " + arg + " No such file or directory");
-<<<<<<< HEAD
                 returnCode = -1;
             }
         }
         return returnCode;
-=======
-                return -1;
-            }
         }
-        return 0;
->>>>>>> 15ad7acafcbad003e5516455dcaea28e09c69a11
-    }
 }
