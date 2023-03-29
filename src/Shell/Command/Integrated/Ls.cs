7fcadd24eaa0dc8989fd5cea2aef5565a0780116
Nikolai <nikolai.stoiko@yandex.ru>
2023-03-29 17:42:41 +0300

    Ls is done

--- src/Shell/Command/Integrated/Ls.cs
+++ src/Shell/Command/Integrated/Ls.cs
@@ -5,11 +5,40 @@ namespace Shell.Command.Integrated;
 public class LsCommand : Command
 {
     public LsCommand(TextReader i, TextWriter o, ShellEnvironment e)
-        : base(i, o, e) { }
+        : base(i, o, e)
+    {
+    }
+
     protected override int Go(string[] args)
     {
-        base.ByCallThisFunctionIConfirmThatThisFunctionChangeBashStateAndThisIsUnsafeActualy();
-        ShellSideEffect = ResultFactory.CreateError<string>("Not implemented");
-        return 0;
+        int returnCode = 0;
+        var path = Env["PWD"];
+        string shift = "./";
+
+        if (args.Length > 0)
+            shift = args[0];
+        path = Path.GetFullPath(Path.Combine(path, shift));
+
+        if (File.Exists(path))
+        {
+            StdOut.WriteLine(String.Join(" ", Path.GetFileName(path)));
+            return 0;
+        }
+
+        if (Directory.Exists(path))
+        {
+            try
+            {
+                List<string> entries = Directory.GetFileSystemEntries(path).ToList();
+                StdOut.WriteLine(String.Join(" ", entries.ConvertAll(Path.GetFileName)));
+                return 0;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                returnCode = -1;
+            }
+        }
+        StdOut.WriteLine("ls: cannot access '" + path + "' No such file or directory");
+        return returnCode;
     }
 }
