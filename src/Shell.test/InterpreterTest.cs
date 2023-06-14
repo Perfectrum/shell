c34d72116006beb56cdf5140f65e3ee22db57cc7
Evgeny <eugenysvirin@gmail.com>
2023-06-14 14:11:21 +0300

    Add more tests

--- src/Shell.test/InterpreterTest.cs
+++ src/Shell.test/InterpreterTest.cs
@@ -14,7 +14,7 @@ using System;
 
 public class InterpeterTest {
     [Fact]
-    public void InterpeterTest1()
+    public void interpeterTest1()
     {
         var interpreter = new Interpreter();
         var res = interpreter.ProcessRequest("echo 2141242");
@@ -36,6 +36,25 @@ public class InterpeterTest {
         Assert.Equal(Status.Exit, res.State);
     }
 
+    [Fact]    
+    public void substitutionTest1()
+    {
+        var interpreter = new Interpreter();
+        var res = interpreter.ProcessRequest("x=1");
+        Assert.Equal(Status.Empty, res.State);
+        
+        res = interpreter.ProcessRequest("echo $x");
+        Assert.Equal(Status.Ok, res.State);
+
+        res = interpreter.ProcessRequest("x=ex");
+        Assert.Equal(Status.Empty, res.State);
+
+        res = interpreter.ProcessRequest("y=it");
+        Assert.Equal(Status.Empty, res.State);
+
+        res = interpreter.ProcessRequest("$x$y");
+        Assert.Equal(Status.Exit, res.State);
+    }
 }
 
 #endif
